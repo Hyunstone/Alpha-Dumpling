bbcc17d31b7511b1a7fc7dac99ca4bfadc20734e
Hyunseok Lee <hyun104467@naver.com>
2022-11-17 02:18:02 +0900

    feat: Greedy 메소드

--- Algorithm/Algorithm.py
+++ Algorithm/Algorithm.py
@@ -9,3 +9,28 @@ class Synergy(Enum):
     
     def getSynergyList():
         return ['APPLE', 'COCAINE', 'BANANA', 'COKE', 'GUN']
+
+userSelectCount = 4
+ingredientNumber = 5
+weightDP = {}
+
+botChoiceList = []
+botSelect = "APPLE"
+weightDP[botSelect] = [-1]
+weightDP['COCAINE'] = [Synergy[botSelect].value['COCAINE']]
+weightDP['BANANA'] = [Synergy[botSelect].value['BANANA']]
+weightDP['COKE'] = [Synergy[botSelect].value['COKE']]
+weightDP['GUN'] = [Synergy[botSelect].value['GUN']]
+
+# 기능: 선택한 식재료를 기반으로 가장 큰 시너지 값을 가진 식재료를 리턴한다
+# 출력: 가장 큰 시너지 값을 가진 식재료
+def Greedy():
+    greedDict = {}
+    for name in Synergy.getSynergyList():
+        greedDict[name] = max(weightDP[name])
+        if (-1 in weightDP[name]): continue
+    result = max(greedDict.values())
+
+    for name in greedDict:
+        if (greedDict[name] == result): 
+            return name
