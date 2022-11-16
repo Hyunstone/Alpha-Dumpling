ec78cd3003c7b0c60601cd9fb46f5152f7f32ad3
Hyunseok Lee <hyun104467@naver.com>
2022-11-17 02:30:54 +0900

    fix: Synergy 클래스 분리

--- Algorithm/Algorithm.py
+++ Algorithm/Algorithm.py
@@ -1,14 +1,4 @@
-from enum import Enum
-
-class Synergy(Enum):
-    APPLE = {"BANANA": 1, "COKE": 3, "GUN": 7, "COCAINE": 2}
-    COCAINE = {"APPLE": 2, "COKE": 4, "BANANA": 7, "GUN": 8}
-    BANANA = {"APPLE": 3, "COCAINE": 7, "COKE": 4, "GUN": 1}
-    COKE = {"APPLE": 3, "COCAINE": 5, "BANANA": 4, "GUN": 3}
-    GUN = {"COCAINE": 2, "BANANA": 1, "COKE": 3, "APPLE": 0}
-    
-    def getSynergyList():
-        return ['APPLE', 'COCAINE', 'BANANA', 'COKE', 'GUN']
+from Synergy.Synergy import *
 
 userSelectCount = 4
 ingredientNumber = 5
